48ab0967943d939c880b83365ca2746a24941e0d
Emrecan Üzüm <uzumemrecan@gmail.com>
2023-01-20 01:58:27 +0300

    Update client.ts

--- client/client.ts
+++ client/client.ts
@@ -26,3 +26,44 @@ const txHash = await pg.connection.sendRawTransaction(tx.serialize());
 console.log(txHash);
 
 //5NufGvLsd3YFfvijWJsWjhJEXzgUuRA53RAgchHU1JyktMcSShTskfGFSeMVoQobm5jgHWUVHhQmDjJkS4Ld5xBB
+
+
+
+//Alternative
+/*
+const programId = new web3.PublicKey(
+  "REPLACE_WITH_YOUR_PROGRAM_ID"
+);
+
+async function sayHello(
+  payer: web3.Keypair
+): Promise<web3.TransactionSignature> {
+  const transaction = new web3.Transaction();
+
+  const instruction = new web3.TransactionInstruction({
+    keys: [], // We're not using any accounts yet
+    programId,
+    // No need to add data here!
+  });
+
+  transaction.add(instruction);
+
+  const transactionSignature = await web3.sendAndConfirmTransaction(
+    pg.connection,
+    transaction,
+    [payer]
+  );
+
+  return transactionSignature;
+}
+
+async function main() {
+  const transactionSignature = await sayHello(pg.wallet.keypair);
+
+  console.log(
+    `Transaction: https://explorer.solana.com/tx/${transactionSignature}?cluster=devnet`
+  );
+}
+
+main();
+*/
